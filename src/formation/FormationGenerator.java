9222cb6e1de4cf9601963d01ee234c57833f9238
Freya <freyahelstrup@me.com>
2019-10-23 10:55:00 +0200

    Added switch per shape

--- src/formation/FormationGenerator.java
+++ src/formation/FormationGenerator.java
@@ -11,11 +11,18 @@ public class FormationGenerator {
         Formation outputFormation = new Formation(new ArrayList<>(), lightPattern, color, durationSeconds);
 
         /* TODO: calculate coordinates based on the shape decided upon. */
-        for (int i = 0; i < numOfDrones; i++){
-            // For now just a simple line
-            outputFormation.coordinates.add(new Coordinate((i%2)*100,i*50,i*10));
+        for (int i = 0; i < numOfDrones; i++) {
+            switch (shape) {
+                case SQUARE:
+                    // For now just a simple line
+                    outputFormation.coordinates.add(new Coordinate((i % 2) * 100, i * 50, i * 10));
+                    break;
+                case CIRCLE:
+                    // For now just a simple line
+                    outputFormation.coordinates.add(new Coordinate((i % 2) * 100, i * (int) Math.PI, i * 10));
+                    break;
+            }
         }
-
         return outputFormation;
     }
 }
